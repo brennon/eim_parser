3a1bb25e03490987a2fb0238e96ed2ccefac7326
Brennon Bortz <brennon@brennonbortz.com>
2014-09-08 12:29:03 -0400

    Missed commit

--- json_injector.py
+++ json_injector.py
@@ -11,10 +11,10 @@ def main():
 
     logger = logging.getLogger('json_injector')
     logger.setLevel(logging.DEBUG)
-    fh = logging.FileHandler('json_injector')
+    fh = logging.FileHandler('json_injector.log')
     fh.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    ch.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
@@ -42,14 +42,43 @@ def main():
     logger.info("Parsing %d .json files below %s" % (len(file_list), root_dir))
     logger.info("Ignoring %d files" % len(ignored_files))
 
+    total_files = len(file_list)
+
     # Iterate over collected files
-    for f in file_list:
+    for (count, f) in enumerate(file_list):
         collection = None
         if re.search('[HRST]\d{3}.json', f):
             collection = 'new_signals'
         else:
             collection = 'new_sessions'
-        call(['mongoimport', '-h', 'muse.cc.vt.edu', '-d', 'eim', '-c', collection, '--file', f])
+            if not check_json_file(f):
+                logger.info("Skipping file (%d/%d) %s" % (count + 1, total_files, f))
+                continue
+
+        logger.info("Importing file (%d/%d) %s" % (count + 1, total_files, f))
+        call(['mongoimport', '-h', 'muse.cc.vt.edu', '-d', 'eim', '-c', collection, '-u', 'brennon', '-p', 'grimier320861!amputate', '--authenticationDatabase', 'admin', '--file', f])
+
+def check_json_file(filepath):
+    valid = True
+    try:
+        f = open(filepath, 'r')
+        j = json.load(f)
+
+        if not 'answers' in j:
+            valid = False
+        if not 'metadata' in j:
+            valid = False
+        if not 'media' in j:
+            valid = False
+        if not 'timestamps' in j:
+            valid = False
+    except:
+        valid = False
+    finally:
+        if f and not f.closed:
+            f.close()
+
+    return valid
 
 if __name__ == "__main__":
     main()
